e7ee1d833a5a5af1f6d5abbaeb48353e1547e74c
enockabere <maebaenock95@gmail>
2021-07-24 20:07:52 +0300

    create a function to collect data from github (inside service)

--- src/app/services/profile.service.ts
+++ src/app/services/profile.service.ts
@@ -1,9 +1,33 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProfileService {
 
-  constructor() { }
+  private username:string;
+  /* 
+    optional properties to help fetch data from github without limitations
+  */
+  private clientId = 'af128be61f6f7314682e';
+  private clientSecret= 'b3cb1151ee09e4c1b4317ee264cf3140bca1b80e';
+
+  //inject http module once imported
+
+  constructor(private http:HttpClient) { 
+    console.log("profile service is now ready");
+    this.username = 'enockabere'; //default profile
+  }
+  //function to collect data from github
+  getUserInfo(){
+    return this.http.get("https://api.github.com/users/" + this.username + "?client_id=" + this.clientId + "&client_secret=" + this.clientSecret)
+    // use .map so that data collect can be in form of observables
+
+    .pipe(map(data =>{})).subscribe(result =>{
+      console.log(result);
+    });
+  }
+    
 }
